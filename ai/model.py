6db1a9eb6f01b14fe2d7ede0d55639bc39260c1e
Sri Harshita Ankolla <ankollasriharshitha@gmail.com>
2026-01-06 20:46:57 +0530

    Update model.py

--- ai/model.py
+++ ai/model.py
@@ -1,1 +1,8 @@
+def predict_recovery(amount, days_overdue):
+    if amount > 10000 and days_overdue < 30:
+        return "High"
+    elif days_overdue < 60:
+        return "Medium"
+    else:
+        return "Low"
 
